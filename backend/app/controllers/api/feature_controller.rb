129493712a8915db41cde23f0982dd9a8be88b2e
Ignacio Orellano <ignaciorellano@hotmail.com>
2024-04-13 20:37:35 -0300

    added serializer to feature

--- backend/app/controllers/api/feature_controller.rb
+++ backend/app/controllers/api/feature_controller.rb
@@ -2,6 +2,8 @@ class Api::FeatureController < ApplicationController
     def index
         @features = Feature.all
 
-        render json: @features
+        render json: {
+            data: ActiveModelSerializers::SerializableResource.new(@features, each_serializer: FeatureSerializer),
+        }
     end
 end
