4390522d44167a02f58f480d256002e370f25c2d
Ignacio Orellano <ignaciorellano@hotmail.com>
2024-04-13 16:53:28 -0300

    added validations to feature model

--- backend/app/models/feature.rb
+++ backend/app/models/feature.rb
@@ -1,2 +1,9 @@
 class Feature < ApplicationRecord
+    validates :place, :mag_type, :title, :external_url, presence: true
+    
+    validates :magnitude, comparison: {greather_than_or_equal_to: -1.0, less_than_or_equal_to: 10.0}
+    validates :c_latitude, comparison: {greather_than_or_equal_to: -90.0, less_than_or_equal_to: 90.0}
+    validates :c_longitude, comparison: {greather_than_or_equal_to: -180.0, less_than_or_equal_to: 180.0}
+
+    validates :external_id, uniqueness: true
 end
